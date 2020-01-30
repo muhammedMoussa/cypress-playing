5454ded79d0d71aef153f81288a3a9b15052fd1f
Muhammed <muhammedmoussa@hotmail.com>
2020-01-30 13:31:06 +0200

    clean

--- cypress/integration/examples/playing.spec.js
+++ cypress/integration/examples/playing.spec.js
@@ -1,3 +1,13 @@
+/**
+ * @TEST_PLAN
+ * Arrange - setup init app state -
+ * - visit a web page
+ * - query for an element
+ * Act - take an action -
+ * - interact with that element
+ * Assert - make an assertion -
+ * - make assertion about page content
+ */
 describe('My Playing Test', function() {
     it('Does not do much', function () {
         expect(true).to.equal(false)
