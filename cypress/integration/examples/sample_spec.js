c558f6dd9c42c5a8b0dbe4961e711f62282ef3cf
Muhammed <muhammedmoussa@hotmail.com>
2020-02-05 13:07:21 +0200

    write real test

---
+++ cypress/integration/examples/sample_spec.js
@@ -0,0 +1,16 @@
+describe('Sample Test', function(){
+    it('Visit web & Click an element & Makes an assertion', function(){
+        cy.visit('https://example.cypress.io/')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url()
+            .should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
+    })
+})
